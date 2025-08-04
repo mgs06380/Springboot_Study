cda2ab5b1301f6856ef5c2c84a952dbf8e82bcd4
mgs06380 <mgs06380@naver.com>
2025-08-04 17:08:19 +0900

    THYMELEAF연동

--- src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -2,6 +2,10 @@ package me.shinsunyoung.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
